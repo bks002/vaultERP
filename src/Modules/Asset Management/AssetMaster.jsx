90829681c2f1555c7bc984934f9715a8ae1752d3
Akshat Singhal <71835769+akshatmikki@users.noreply.github.com>
2025-07-26 11:58:09 +0530

    asset working (#15)
    * Created Asset Master and in the Asset Master we have created a form and I have Created Asset Operation and Asset Type Master
    
    * Removed a text
    
    * fixed all changes
    
    * Added Search button icon and added a dropdown form in user
    
    * Added a dropdown in asset master
    
    * changes
    
    * changes
    
    * asset changes
    
    * asset master and ops
    
    * po fixed
    
    * created assetservice.js file
    
    * shift master changes
    
    * employee and shift
    
    * changes
    
    * Created Shift Master Page and made it responsive
    
    * vendor changes
    
    * Added search icon in all
    
    * Again fixed some error
    
    * Created a job card
    
    * conflicts resolved
    
    * po price button change
    
    * po changes
    
    * API call done in Party Master and Product Master edit save and delete (#13)
    
    Co-authored-by: Aman Singh <amansinghkld@gmail.com>
    
    * work order
    
    * workorder jobcard party and product master page
    
    * asset working
    
    ---------
    
    Co-authored-by: Aman Singh <amansinghkld@gmail.com>
    Co-authored-by: bks002 <kbhaveshsingh@gmail.com>
    Co-authored-by: Bhavesh Kumar Singh <98304423+bks002@users.noreply.github.com>

--- src/Modules/Asset Management/AssetMaster.jsx
+++ src/Modules/Asset Management/AssetMaster.jsx
@@ -10,7 +10,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import SettingsIcon from '@mui/icons-material/Settings';
-// import {assetTypes} from "../../Components/constant";
+import { assetTypes } from "../../Components/constant";
 import { getAllAssets, createAssets, EditAssets, deleteAsset } from "../../Services/AssetService";
 import { getAllOperation } from "../../Services/OperationService";
 import { getAssetOperation, OperationMapping } from "../../Services/AssetOperation";
@@ -122,8 +122,6 @@ const AssetMaster = () => {
     }
   };
 
- 
-
   const handleCheckboxChange = (assetId) => {
     setSelectedIds((prev) =>
       prev.includes(assetId)
@@ -152,17 +150,7 @@ const AssetMaster = () => {
 
   const handleCreate = () => {
     setIsEdit(false);
-    setFormData({
-      assetCode: "",
-      assetName: "",
-      assetTypeId: "",
-      manufacturer: "",
-      modelNumber: "",
-      serialNumber: "",
-      purchaseDate: "",
-      warrantyExpiry: "",
-      supplier: "",
-    });
+    setFormData(defaultFormData);
     setDialogOpen(true);
   };
 
@@ -182,36 +170,37 @@ const AssetMaster = () => {
       }
     }
   };
-    const filteredAssets = assets.filter((v) =>
-        v.assetName?.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        v.manufacturer?.toLowerCase().includes(searchQuery.toLowerCase())
- );
+
+  const filteredAssets = assets.filter((v) =>
+    v.assetName?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    v.manufacturer?.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <Container maxWidth={false}>
       {/* Header */}
       <Box display="flex" justifyContent="space-between" alignItems="center" sx={{ mb: 2 }}>
-                <Typography variant="h4">Asset Master</Typography>
-                <Box display="flex" alignItems="center" gap={2}>
-                    <TextField
-                        placeholder="Search by Asset name, Manufacturer"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <SearchIcon />
-                                </InputAdornment>
-                            ),
-                        }}
-                        size="small"
-                        sx={{ width: 300 }}
-                    />
-                    <Button variant="contained" color="primary" onClick={handleCreate}>
-                        Add Asset Master
-                    </Button>
-                </Box>
-            </Box>
-
+        <Typography variant="h4">Asset Master</Typography>
+        <Box display="flex" alignItems="center" gap={2}>
+          <TextField
+            placeholder="Search by Asset name, Manufacturer"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+            size="small"
+            sx={{ width: 300 }}
+          />
+          <Button variant="contained" color="primary" onClick={handleCreate}>
+            Add Asset Master
+          </Button>
+        </Box>
+      </Box>
 
       {/* Table */}
       {loading ? (
@@ -265,8 +254,8 @@ const AssetMaster = () => {
 
             <Grid item xs={12} md={6} size={6}>
               <TextField fullWidth label="Asset Code" name="assetCode" value={formData.assetCode} onChange={handleChange} />
-              <TextField select fullWidth label="Asset Type Id" name="assetName" type="number" value={formData.assetName} onChange={handleChange} sx={{ mt: 2 }} >
-                {getAllAssets.map((type) => (
+              <TextField select fullWidth label="Asset Type" name="assetTypeId" value={formData.assetTypeId} onChange={handleChange} sx={{ mt: 2 }} >
+                {assetTypes.map((type) => (
                   <MenuItem key={type.id} value={type.id}>
                     {type.type}
                   </MenuItem>
